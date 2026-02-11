93c9573decd125c826920e7eb7592f7123667fed
Petra-78 <partos.petra.0305@gmail.com>
2026-02-11 18:06:19 +0100

    Switch app routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import passport from "./config/jwtStrategy.js"
+import passport from "./config/jwtStrategy.js";
 
 import { authRouter } from "./routes/authRouter.js";
 
@@ -8,11 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(passport.initialize());
 
-app.use("/", passport.authenticate("jwt", { session: false }), (req,res)=>{
-    res.json({message:"hi!"})
-})
-
-app.use("/", authRouter)
+app.use("/", authRouter);
+app.use("/", passport.authenticate("jwt", { session: false }), (req, res) => {
+  res.json({ message: "hi!" });
+});
 
 app.listen(process.env.PORT, (error) => {
   if (error) {
