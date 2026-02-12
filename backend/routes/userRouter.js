3f6fab6a99521257e9a8b4264e08ee55680b8fb8
Petra-78 <partos.petra.0305@gmail.com>
2026-02-12 14:22:15 +0100

    Add recent, popular and profile update route

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,13 +1,20 @@
 import { Router } from "express";
 const router = Router();
 import {
+  getPopularUsers,
   getProfile,
+  getRecentUsers,
   getUser,
   getUsers,
+  updateUser,
 } from "../controllers/userController.js";
 
 router.get("/", getUsers);
 router.get("/me", getProfile);
+router.get("/recent", getRecentUsers);
+router.get("/popular", getPopularUsers);
 router.get("/:username", getUser);
+
+router.put("/me/update", updateUser);
 
 export { router as userRouter };
