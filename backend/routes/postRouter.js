5a8709471a7478896f149780d313a24d9a759cc3
Petra-78 <partos.petra.0305@gmail.com>
2026-02-12 19:07:28 +0100

    Add post like request

--- backend/routes/postRouter.js
+++ backend/routes/postRouter.js
@@ -4,12 +4,14 @@ import {
   getPosts,
   getPostsOfFollowing,
   getUsersPosts,
+  likePost,
 } from "../controllers/postController.js";
 const router = Router();
 
 router.get("/", getPosts);
 router.get("/following", getPostsOfFollowing);
 router.get("/:postId", getPost);
+router.post("/:postId/like", likePost);
 router.get("/user/:username", getUsersPosts);
 
 export { router as postRouter };
