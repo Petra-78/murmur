6974b311d80189bf8c4233e2fdfa1aaa915efefa
Petra-78 <partos.petra.0305@gmail.com>
2026-02-12 15:30:11 +0100

    Add user follow and unfollow requests

--- backend/routes/followRouter.js
+++ backend/routes/followRouter.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { getFollowers, getFollowing } from "../controllers/followController.js";
+import {
+  followUser,
+  getFollowers,
+  getFollowing,
+  unfollowUser,
+} from "../controllers/followController.js";
 const router = Router();
 
 router.get("/:username/followers", getFollowers);
 router.get("/:username/following", getFollowing);
+
+router.post("/:username/follow", followUser);
+router.delete("/:username/unfollow", unfollowUser);
 
 export { router as followerRouter };
