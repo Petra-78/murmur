d7f12cd3bd9b73f4637cb80704393b78db5fcca5
Petra-78 <partos.petra.0305@gmail.com>
2026-02-13 14:20:28 +0100

    Add pfp updating request

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 const router = Router();
+import { upload } from "../config/multer.js";
 import {
   getPopularUsers,
   getProfile,
@@ -7,6 +8,7 @@ import {
   getUser,
   getUsers,
   updateUser,
+  uploadProfilePicture,
 } from "../controllers/userController.js";
 
 router.get("/", getUsers);
@@ -16,5 +18,6 @@ router.get("/popular", getPopularUsers);
 router.get("/:username", getUser);
 
 router.put("/me/update", updateUser);
+router.put("/me/update/pfp", upload.single("file"), uploadProfilePicture);
 
 export { router as userRouter };
