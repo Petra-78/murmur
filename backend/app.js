edbc2f4b6ced4c1b70895b39f1a2ca5cc3bf7665
Petra-78 <partos.petra.0305@gmail.com>
2026-02-14 00:02:32 +0100

    Integrate socket io + add chat router

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,38 @@
 import express from "express";
 import passport from "./config/jwtStrategy.js";
 import "dotenv/config";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 import { authRouter } from "./routes/authRouter.js";
 import { userRouter } from "./routes/userRouter.js";
 import { followRouter } from "./routes/followRouter.js";
 import { postRouter } from "./routes/postRouter.js";
 import { commentRouter } from "./routes/commentRouter.js";
+import { chatRouter } from "./routes/chatRouter.js";
 
-const app = express();
+export const app = express();
 
 app.use(express.json());
 app.use(passport.initialize());
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "test.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 app.use("/", authRouter);
 
@@ -29,8 +50,9 @@ app.use(
   passport.authenticate("jwt", { session: false }),
   commentRouter,
 );
+app.use("/chats", passport.authenticate("jwt", { session: false }), chatRouter);
 
-app.listen(process.env.PORT, (error) => {
+server.listen(process.env.PORT, (error) => {
   if (error) {
     throw error;
   }
