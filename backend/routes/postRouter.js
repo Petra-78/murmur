e08a6f3937cc2bdf493ee1f489596c17b62454c6
Petra-78 <partos.petra.0305@gmail.com>
2026-02-13 13:26:48 +0100

    Add post create and delete request

--- backend/routes/postRouter.js
+++ backend/routes/postRouter.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { upload } from "../config/multer.js";
 import {
+  deletePost,
   getLikedPosts,
   getPost,
   getPosts,
   getPostsOfFollowing,
   getUsersPosts,
   likePost,
+  uploadPost,
 } from "../controllers/postController.js";
 const router = Router();
 
@@ -15,5 +18,9 @@ router.get("/liked", getLikedPosts);
 router.get("/:postId", getPost);
 router.post("/:postId/like", likePost);
 router.get("/user/:username", getUsersPosts);
+
+router.post("/", upload.single("file"), uploadPost);
+
+router.delete("/:postId", deletePost);
 
 export { router as postRouter };
