f296d3e1ae3a4ab2c3b15578a74cc514b16a9c75
Petra-78 <partos.petra.0305@gmail.com>
2026-02-17 20:07:47 +0100

    Run tailwind prettier

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useAuth } from "../context/authContext";
 import { Navigate } from "react-router-dom";
-import PostCard from "../components/posts/PostCard";
+import PostCards from "../components/posts/PostCards";
 import { useState } from "react";
 
 export default function Home() {
@@ -11,44 +11,38 @@ export default function Home() {
   if (!user) return <Navigate to="/login" replace />;
 
   return (
-    <div className="h-dvh w-dvw flex flex-col bg-gray-100 dark:bg-zinc-900 overflow-hidden p-1 items-center">
+    <div className="flex h-dvh w-dvw flex-col items-center overflow-hidden bg-gray-100 p-1 dark:bg-zinc-900">
       <div className="flex justify-center gap-8 py-6">
         <button
           onClick={() => setFeed("posts")}
-          className={` px-2
-      relative pb-2 text-md transition-colors duration-300
-      ${
-        feed === "posts"
-          ? "font-bold dark:text-white text-zinc-900 "
-          : "text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200"
-      }
-    `}
+          className={`text-md relative px-2 pb-2 transition-colors duration-300 ${
+            feed === "posts"
+              ? "font-bold text-zinc-900 dark:text-white"
+              : "text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200"
+          } `}
         >
           Posts
           {feed === "posts" && (
-            <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#A13333] rounded-full" />
+            <span className="absolute bottom-0 left-0 h-0.5 w-full rounded-full bg-[#A13333]" />
           )}
         </button>
 
         <button
           onClick={() => setFeed("following")}
-          className={`
-      relative pb-2 text-md  transition-colors duration-300 px-2
-      ${
-        feed === "following"
-          ? "font-bold dark:text-white text-zinc-900"
-          : "text-gray-500 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200"
-      }
-    `}
+          className={`text-md relative px-2 pb-2 transition-colors duration-300 ${
+            feed === "following"
+              ? "font-bold text-zinc-900 dark:text-white"
+              : "text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200"
+          } `}
         >
           Following
           {feed === "following" && (
-            <span className="absolute left-0 bottom-0 w-full h-0.5 bg-[#A13333] rounded-full" />
+            <span className="absolute bottom-0 left-0 h-0.5 w-full rounded-full bg-[#A13333]" />
           )}
         </button>
       </div>
 
-      <PostCard feed={feed} />
+      <PostCards feed={feed} />
     </div>
   );
 }
