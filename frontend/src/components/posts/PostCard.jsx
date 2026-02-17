e0b441519bc099e618ada1bd669f52b88ee572b4
Petra-78 <partos.petra.0305@gmail.com>
2026-02-17 20:10:23 +0100

    Add going back button + rename file

--- frontend/src/components/posts/PostCard.jsx
+++ frontend/src/components/posts/PostCard.jsx
@@ -1,109 +1,102 @@
 import { useEffect, useState } from "react";
-import { useAuth } from "../../context/authContext";
-import { formatDate } from "../../utils/dateFormatter";
+import Loading from "../Loading";
 import LikeButton from "../buttons/LikeButton";
 import CommentButton from "../buttons/CommentButton";
+import { useParams } from "react-router";
+import { useAuth } from "../../context/authContext";
+import { formatDate } from "../../utils/dateFormatter";
+import { useNavigate } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
-export default function PostCard({ feed }) {
-  const { token, user } = useAuth();
+export default function PostCard() {
+  const navigate = useNavigate();
+  const { postId } = useParams();
+  const { token } = useAuth();
   const [loading, setLoading] = useState(true);
-  const [posts, setPosts] = useState(null);
+  const [post, setPost] = useState(null);
 
   useEffect(() => {
-    async function fetchPosts() {
+    if (!token) return;
+    async function fetchPost() {
       setLoading(true);
-      let url;
-      if (feed === "posts")
-        url = "https://murmur-production.up.railway.app/posts";
-      if (feed === "following")
-        url =
-          "https://murmur-production.up.railway.app/posts/following";
-      try {
-        const res = await fetch(url, {
+      const res = await fetch(
+        `https://murmur-production.up.railway.app/posts/${postId}`,
+        {
           method: "GET",
           headers: {
             Authorization: `Bearer ${token}`,
           },
-        });
-        if (!res.ok) {
-          console.error("Failed to fetch posts");
-          return;
-        }
-        const data = await res.json();
-        setPosts(data);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
+        },
+      );
+
+      const data = await res.json();
+      if (!res.ok) {
+        console.error(data.message || "Failed to fetch post");
       }
+      setPost(data);
+      setLoading(false);
     }
+    fetchPost();
+  }, [token, postId]);
 
-    fetchPosts();
-  }, [feed]);
-
-  if (loading)
-    return (
-      <div className="flex flex-1 items-center justify-center">
-        <FontAwesomeIcon
-          className="text-5xl text-[#A13333]"
-          icon={faSpinner}
-          spin={true}
-        />
-      </div>
-    );
-
-  if (posts.length === 0)
-    return <p className="dark:text-white">No posts yet.</p>;
+  if (loading) return <Loading />;
 
   return (
     <>
-      {posts.map((post) => (
-        <div
-          key={post.id}
-          className="relative flex w-full max-w-2xl cursor-pointer flex-col gap-3 rounded-2xl bg-white/90 p-4 shadow-lg shadow-red-800/20 transition-all duration-300 hover:shadow-2xl dark:bg-[#040303]/90"
+      <div className="w-full border-b border-[#A13333] bg-white/90 p-3 dark:bg-[#040303]/90">
+        <button
+          className="flex items-center gap-3 text-zinc-900 transition-colors duration-200 hover:text-[#A13333] dark:text-gray-300"
+          onClick={() => navigate(-1)}
         >
-          <div className="flex items-center gap-2">
-            <img
-              className="h-12 w-12 rounded-full border border-[#e5d6d3] dark:border-[#A13333]/50"
-              src={post.author.profileUrl || "/placeholder.jpeg"}
-              alt="profile picture"
-            />
-            <div className="flex items-center gap-1 text-sm text-gray-700 dark:text-gray-300">
-              <p className="font-semibold">{post.author.username}</p>
-              <span>·</span>
-              <span className="text-xs">
-                {formatDate(post.createdAt)}
-              </span>
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            {post.content && (
-              <p className="px-2 text-left text-lg text-gray-800 dark:text-gray-100">
-                {post.content}
-              </p>
-            )}
-            {post.imageUrl && (
-              <img
-                src={post.imageUrl}
-                alt="post image"
-                className="max-h-96 w-full rounded-xl object-cover"
-              />
-            )}
-          </div>
-
-          <div className="mt-2 flex items-center gap-3">
-            <LikeButton
-              type="post"
-              id={post.id}
-              likes={post._count.likes}
-              isLiked={post.likes.length > 0 ? true : false}
-            />
-            <CommentButton comments={post._count.comments} />
+          <FontAwesomeIcon icon={faArrowLeft} />
+          Back to posts
+        </button>
+      </div>
+      <div
+        key={post.id}
+        className="relative flex w-full flex-col gap-3 bg-white/90 p-4 shadow-lg shadow-red-800/20 transition-all duration-300 hover:shadow-2xl dark:bg-[#040303]/90"
+      >
+        <div className="flex items-center gap-2">
+          <img
+            className="h-10 w-10 rounded-full border border-[#e5d6d3] dark:border-[#A13333]/50"
+            src={post.author.profileUrl || "/placeholder.jpeg"}
+            alt="profile picture"
+          />
+          <div className="flex items-center gap-1 text-sm text-gray-700 dark:text-gray-300">
+            <p className="font-semibold">{post.author.username}</p>
+            <span>·</span>
+            <span className="text-xs">
+              {formatDate(post.createdAt)}
+            </span>
           </div>
         </div>
-      ))}
+
+        <div className="flex flex-col gap-2">
+          {post.content && (
+            <p className="px-2 text-left text-lg text-gray-800 dark:text-gray-100">
+              {post.content}
+            </p>
+          )}
+          {post.imageUrl && (
+            <img
+              src={post.imageUrl}
+              alt="post image"
+              className="max-h-96 w-full rounded-xl object-cover"
+            />
+          )}
+        </div>
+
+        <div className="mt-2 flex items-center gap-3">
+          <LikeButton
+            type="post"
+            id={post.id}
+            likes={post._count.likes}
+            isLiked={post.likes.length > 0 ? true : false}
+          />
+          <CommentButton comments={post._count.comments} />
+        </div>
+      </div>
     </>
   );
 }
