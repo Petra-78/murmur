4b1f0fb4876fa2783c459df32b6fdb5fb9608c4b
Petra-78 <partos.petra.0305@gmail.com>
2026-02-18 16:10:33 +0100

    Add theme css

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -3,7 +3,7 @@ import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export default function Loading() {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex w-full flex-1 items-center justify-center bg-gray-100 dark:bg-zinc-900">
       <FontAwesomeIcon
         className="text-5xl text-[#A13333]"
         icon={faSpinner}
