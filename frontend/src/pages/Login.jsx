cc55cec99b75ee47a9f515043d9156960bd9dfe7
Petra-78 <partos.petra.0305@gmail.com>
2026-02-22 19:55:24 +0100

    Add guest login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -42,61 +42,56 @@ export default function Login() {
       setError(err.message);
     }
   };
+
+  const handleGuestSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      const res = await fetch(
+        "https://murmur-production.up.railway.app/login/guest",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        },
+      );
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.message);
+      }
+
+      login(data.token, data.user);
+
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+      setError(err.message);
+    }
+  };
   return (
-    <div
-      className="
-    relative flex-1 flex items-center justify-center overflow-hidden p-4 transition-colors duration-500
-    bg-linear-to-br 
-    from-[#fdf8f6] via-[#f7e9e8] to-[#f3ece7]
-    dark:from-[#040303] dark:via-[#1a0a0a] dark:to-[#040303]
-  "
-    >
-      <div
-        className="
-  absolute w-80 h-80 rounded-full blur-3xl opacity-40 top-12 -left-8
-  bg-linear-to-tr bg-[#FADADD]/20
+    <div className="relative flex flex-1 items-center justify-center overflow-hidden bg-linear-to-br from-[#fdf8f6] via-[#f7e9e8] to-[#f3ece7] p-4 transition-colors duration-500 dark:from-[#040303] dark:via-[#1a0a0a] dark:to-[#040303]">
+      <div className="absolute top-12 -left-8 h-80 w-80 rounded-full bg-[#FADADD]/20 bg-linear-to-tr opacity-40 blur-3xl" />
 
-"
-      />
+      <div className="absolute right-10 -bottom-10 h-72 w-72 rounded-full bg-[#FFE5B4]/30 bg-linear-to-br opacity-30 blur-3xl" />
 
-      <div
-        className="
-  absolute w-72 h-72 rounded-full blur-3xl opacity-30 -bottom-10 right-10
-  bg-linear-to-br bg-[#FFE5B4]/30 
-
-"
-      />
-
-      <div
-        className="
-      relative rounded-3xl w-full max-w-md p-10 
-      transition-colors duration-500
-
-      bg-white/80 border border-[#e5d6d3] text-gray-800
-      dark:bg-[#040303]/50 dark:border-[#A13333]/30 dark:text-gray-100
-    "
-      >
-        <h2 className="text-3xl font-semibold tracking-wide text-center mb-9">
+      <div className="relative w-full max-w-md rounded-3xl border border-[#e5d6d3] bg-white/80 p-10 text-gray-800 transition-colors duration-500 dark:border-[#A13333]/30 dark:bg-[#040303]/50 dark:text-gray-100">
+        <h2 className="mb-9 text-center text-3xl font-semibold tracking-wide">
           Welcome to Murmur
         </h2>
 
-        <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+        <form className="flex flex-col gap-5">
           <input
             type="email"
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
-            className="
-          px-4 py-3 rounded-xl transition outline-none
-
-          bg-white border border-[#d9c2be] placeholder-gray-400
-          focus:ring-2 focus:ring-[#B3541E] focus:border-transparent
-
-          dark:bg-[#2a0f0f] dark:border-[#A13333]/40 
-          dark:placeholder-gray-500 
-          dark:focus:ring-[#A13333]
-          "
+            className="rounded-xl border border-[#d9c2be] bg-white px-4 py-3 placeholder-gray-400 transition outline-none focus:border-transparent focus:ring-2 focus:ring-[#B3541E] dark:border-[#A13333]/40 dark:bg-[#2a0f0f] dark:placeholder-gray-500 dark:focus:ring-[#A13333]"
           />
 
           <input
@@ -105,49 +100,38 @@ export default function Login() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-            className="
-          px-4 py-3 rounded-xl transition outline-none
-
-          bg-white border border-[#d9c2be] placeholder-gray-400
-          focus:ring-2 focus:ring-[#B3541E] focus:border-transparent
-
-          dark:bg-[#2a0f0f] dark:border-[#A13333]/40 
-          dark:placeholder-gray-500 
-          dark:focus:ring-[#A13333]
-          "
+            className="rounded-xl border border-[#d9c2be] bg-white px-4 py-3 placeholder-gray-400 transition outline-none focus:border-transparent focus:ring-2 focus:ring-[#B3541E] dark:border-[#A13333]/40 dark:bg-[#2a0f0f] dark:placeholder-gray-500 dark:focus:ring-[#A13333]"
           />
 
           {error && (
-            <p className="text-red-500 dark:text-[#A13333] text-sm mb-4 text-left">
+            <p className="mb-4 text-left text-sm text-red-500 dark:text-[#A13333]">
               {error}
             </p>
           )}
 
           <button
+            onClick={(e) => handleSubmit(e)}
             type="submit"
-            className="
-          mt-2 py-3 rounded-xl font-medium tracking-wide transition transform shadow-lg
-
-          bg-linear-to-r from-[#B3541E] to-[#A13333] text-white
-          hover:scale-[1.02] active:scale-[0.98]
-
-          dark:from-[#A13333] dark:to-[#B3541E]
-          "
+            className="mt-2 transform rounded-xl bg-linear-to-r from-[#B3541E] to-[#A13333] py-3 font-medium tracking-wide text-white shadow-lg transition hover:scale-[1.02] active:scale-[0.98] dark:from-[#A13333] dark:to-[#B3541E]"
           >
             Login
           </button>
+
+          <p>OR</p>
+          <button
+            onClick={(e) => handleGuestSubmit(e)}
+            type="submit"
+            className="mt-2 transform rounded-xl bg-[#d43939] py-3 font-medium tracking-wide text-white shadow-lg transition hover:scale-[1.02] active:scale-[0.98]"
+          >
+            Login as Guest
+          </button>
         </form>
 
-        <p className="text-sm mt-6 text-center text-gray-600 dark:text-gray-400">
+        <p className="mt-6 text-center text-sm text-gray-600 dark:text-gray-400">
           Don't have an account?{" "}
           <Link
             to={"/signup"}
-            className="
-          font-medium transition
-
-          text-[#A13333] hover:text-[#B3541E]
-          dark:text-[#B3541E] dark:hover:text-[#A13333]
-          "
+            className="font-medium text-[#A13333] transition hover:text-[#B3541E] dark:text-[#B3541E] dark:hover:text-[#A13333]"
           >
             Sign up!
           </Link>
