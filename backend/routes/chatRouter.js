da1ea32ed549a0f567d651b9480fa56e04e274af
Petra-78 <partos.petra.0305@gmail.com>
2026-02-23 17:06:44 +0100

    Add file upload

--- backend/routes/chatRouter.js
+++ backend/routes/chatRouter.js
@@ -4,11 +4,12 @@ import {
   getMessages,
   sendMessage,
 } from "../controllers/chatController.js";
+import { upload } from "../config/multer.js";
 const router = Router();
 
 router.get("/", getChats);
 router.get("/messages/:selectedUserId", getMessages);
 
-router.post("/messages", sendMessage);
+router.post("/messages", upload.single("file"), sendMessage);
 
 export { router as chatRouter };
