a5bac449114494574029c6e9385bd4e8fc3b1d01
Petra-78 <partos.petra.0305@gmail.com>
2026-02-24 18:31:03 +0100

    Add deployed frontend url to cors

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://murmur-app.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
